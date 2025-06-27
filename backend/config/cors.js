d0a143fa602893a6599ae5c4df1b0736de62b7f0
Do Hoai Nam <namdhpi00092@gmail.com>
2025-06-27 15:00:45 +0700

    fix cart, checkout, seller-order

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["http://localhost:5500", "http://127.0.0.1:5500", "https://t5-market.vercel.app"],
+  origin: ["http://localhost:5500", "http://127.0.0.1:5500", "https://t5-market.vercel.app" , "http://localhost:5501", "http://127.0.0.1:5501"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
