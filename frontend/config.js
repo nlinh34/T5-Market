58986c65b3380a640070d625205536860b6a7752
nlinh34 <linhhnnpi00217@gmail.com>
2025-07-01 13:15:35 +0700

    fix config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:5000"
-    : "https://t5-market.vercel.app"; // Thay bằng domain backend bạn đã deploy (Railway/Render/Vercel)
+    : "https://t5-market.onrender.com/"; // Thay bằng domain backend bạn đã deploy (Railway/Render/Vercel)
