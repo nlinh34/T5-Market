25f0fee7c6ba09d04c8f70557bb42e88993ad5f4
nlinh34 <linhhnnpi00217@gmail.com>
2025-07-11 10:52:13 +0700

    add api cart

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,52 +1,31 @@
+// models/Cart.js
 const mongoose = require("mongoose");
 
 const cartItemSchema = new mongoose.Schema({
-  product_id: {
+  product: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
     required: true
   },
   quantity: {
     type: Number,
-    required: true,
     default: 1,
     min: 1
   }
 });
 
-const CartSchema = new mongoose.Schema(
-  {
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      unique: true
-    },
-    items: [cartItemSchema], // Danh sách sản phẩm trong giỏ hàng
-
-    voucher_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Voucher",
-      default: null
-    },
-    subtotal: {
-      type: Number,
-      default: 0
-    },
-    shipping_fee: {
-      type: Number,
-      default: 15000
-    },
-    discount_amount: {
-      type: Number,
-      default: 0
-    },
-    total_amount: {
-      type: Number,
-      default: 0
-    }
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true // mỗi user có 1 cart
   },
-  { timestamps: true }
-);
+  items: [cartItemSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-module.exports = mongoose.model("Cart", CartSchema);
+module.exports = mongoose.model("Cart", cartSchema);
